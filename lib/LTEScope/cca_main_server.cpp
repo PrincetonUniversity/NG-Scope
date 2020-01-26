39869dba5cc43494ac57eb3403019e2b9953964d
alex <yaxiongx@princeton.edu>
2020-01-25 21:47:14 -0500

    new AWS IP

--- lib/LTEScope/cca_main_server.cpp
+++ lib/LTEScope/cca_main_server.cpp
@@ -109,7 +109,7 @@ int main(int argc, char **argv) {
     int con_time_s  = 25;
     int con_time_ns = 0;
     char AWS_servIP[100];
-    strcpy(AWS_servIP, "3.15.138.170");
+    strcpy(AWS_servIP, "13.58.103.15");
 
     // Connection (with USRP PC) parameters 
     int server_sock;
