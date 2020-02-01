9cf1b05bbb00df149212e80211efa8fccf30c174
alex <yaxiongx@princeton.edu>
2020-01-31 23:55:34 -0500

    from bin folder

--- lib/LTEScope/scripts/mv_lteCCA_trace.sh
+++ lib/LTEScope/scripts/mv_lteCCA_trace.sh
@@ -2,6 +2,7 @@
 # PC IP of where to store the traces
 #trPC="yaxiongxie@192.168.1.200"       # PC IP
 trPC="alex@192.168.1.52"       # PC IP
+servPCName="paws"
 #servIP="18.220.169.58"
 servIP=$2
 #trFolder="~/FanYi/re_PAWS/CCA/static_data"  # Folder to store
@@ -21,25 +22,41 @@ eval "$cmd"
 cmd="scp ubuntu@$servIP:~/cca_server/server_tick_log ./data/"
 eval "$cmd"
 
-cmd="scp alex@192.168.1.20:~/LTEScope/build/lib/LTEScope/client_log ./data"
+cmd="scp $servPCName@192.168.2.20:~/LTEScope/build/lib/LTEScope/client_log ./data"
 eval "$cmd"
 
-cmd="scp alex@192.168.1.20:~/LTEScope/build/lib/LTEScope/CCA_rate_log ./data"
+cmd="scp $servPCName@192.168.2.20:~/LTEScope/build/lib/LTEScope/CCA_rate_log ./data"
 eval "$cmd"
 
-cmd="scp ./data/server_ack_log $trPC:$trFolder/TR_$1/lteCCA_trace/"
+cmd="scp ./data/server_ack_log $trPC:$trFolder/TR_$1/lteCCA_trace/server_ack_log_3usrp"
 eval "$cmd"
 
-cmd="scp ./data/server_tick_log $trPC:$trFolder/TR_$1/lteCCA_trace/"
+cmd="scp ./data/server_tick_log $trPC:$trFolder/TR_$1/lteCCA_trace/server_tick_log_3usrp"
 eval "$cmd"
 
-cmd="scp ./data/client_log $trPC:$trFolder/TR_$1/lteCCA_trace/"
+cmd="scp ./data/client_log $trPC:$trFolder/TR_$1/lteCCA_trace/client_log_3usrp"
 eval "$cmd"
 
-cmd="scp ./data/CCA_rate_log $trPC:$trFolder/TR_$1/lteCCA_trace/"
+cmd="scp ./data/CCA_rate_log $trPC:$trFolder/TR_$1/lteCCA_trace/CCA_rate_log_3usrp"
 eval "$cmd"
 
-cmd="scp ./dci_log $trPC:$trFolder/TR_$1/lteCCA_trace/"
+cmd="scp ./dci_log $trPC:$trFolder/TR_$1/lteCCA_trace/dci_log_3usrp"
 eval "$cmd"
 
+cmd="scp ./dci_dl_usrp_0_dci_1.txt $trPC:$trFolder/TR_$1/lteCCA_trace/dci_dl_usrp_0_dci_1_3usrp.txt"
+eval "$cmd"
 
+cmd="scp ./dci_dl_usrp_0_dci_2.txt $trPC:$trFolder/TR_$1/lteCCA_trace/dci_dl_usrp_0_dci_2_3usrp.txt"
+eval "$cmd"
+
+cmd="scp ./dci_dl_usrp_1_dci_1.txt $trPC:$trFolder/TR_$1/lteCCA_trace/dci_dl_usrp_1_dci_1_3usrp.txt"
+eval "$cmd"
+
+cmd="scp ./dci_dl_usrp_1_dci_2.txt $trPC:$trFolder/TR_$1/lteCCA_trace/dci_dl_usrp_1_dci_2_3usrp.txt"
+eval "$cmd"
+
+cmd="scp ./dci_dl_usrp_2_dci_1.txt $trPC:$trFolder/TR_$1/lteCCA_trace/dci_dl_usrp_2_dci_1_3usrp.txt"
+eval "$cmd"
+
+cmd="scp ./dci_dl_usrp_2_dci_2.txt $trPC:$trFolder/TR_$1/lteCCA_trace/dci_dl_usrp_2_dci_2_3usrp.txt"
+eval "$cmd"
