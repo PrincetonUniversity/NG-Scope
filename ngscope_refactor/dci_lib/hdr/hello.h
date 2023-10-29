fe4c4df83be2239ab39f6d21f255fccde96ef6fb
yaxiong <xieyaxiongfly@gmail.com>
2023-10-29 15:56:14 -0400

    structure

---
+++ ngscope_refactor/dci_lib/hdr/hello.h
@@ -0,0 +1,4 @@
+#include <stdio.h>
+
+
+int hello_world();
---
+++ ngscope_refactor/dci_lib/src/hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "ngscope_refactor/dci_lib/hdr/hello.h"
+
+int hello_world(){
+	printf("Hello world!\n");
+	return 0;
+}
